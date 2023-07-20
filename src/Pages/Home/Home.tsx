5cfc9bf9dfc84fa366166d7c97a0fd315dd54587
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-07-20 12:35:24 -0500

    add useEffect to control title

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 type Props = {};
 
-const Home = (props: Props) => {
+const Home: React.FC = (props: Props) => {
+  useEffect(() => {
+    document.title = "AYN Services"; // Change "New Tab Title" to whatever you want the new title to be.
+  }, []);
   return <div>Home</div>;
 };
 
