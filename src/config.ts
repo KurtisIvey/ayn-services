2caecc409632a874ddb80b66494c79a1afc52863
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-07-20 22:56:45 -0500

    add address

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 const config = {
   phoneNumber: "1-469-534-6406",
   email: "scott@aynservices.com",
+  address: "16527 Addison Road",
+  cityStateZip: "Addison, TX 75001",
 };
 
 export default config;
